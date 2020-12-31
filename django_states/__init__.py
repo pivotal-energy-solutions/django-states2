d816ccd96bd451b695f8b97b25c8f65b12fd1611
Steven K <steven@pointcircle.com>
2020-12-31 16:18:36 -0700

    Bump

--- django_states/__init__.py
+++ django_states/__init__.py
@@ -6,14 +6,14 @@ Define a state graph for a model and remember the state of each object.
 State transitions can be logged for objects.
 """
 
-
 __name__ = 'django_states'
 __author__ = 'Pivotal Energy Solutions'
-__version_info__ = (1, 7, 0)
+__version_info__ = (1, 7, 2)
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '2014/07/22 4:47:00 PM'
-__credits__ = ['Jonathan Slenders', 'Ben Mason', 'Dirk Moors', 'Gert Van Gool', 'Giovanni Collazo', 'Jakub Paczkowski',
-               'Jan Fabry', 'Jef Geskens', 'Jonathan Slenders', 'José Padilla', 'Linsy Aerts',
-               'Maarten Timmerman', 'Niels Van Och', 'Olivier Sels', 'OpenShift guest', 'San Gillis',
-               'Simon Andersson', 'Steven Klass', 'sgillis', 'techdragon']
+__credits__ = ['Jonathan Slenders', 'Ben Mason', 'Dirk Moors', 'Gert Van Gool', 'Giovanni Collazo',
+               'Jakub Paczkowski', 'Jan Fabry', 'Jef Geskens', 'Jonathan Slenders', 'José Padilla',
+               'Linsy Aerts', 'Maarten Timmerman', 'Niels Van Och', 'Olivier Sels',
+               'OpenShift guest', 'San Gillis', 'Simon Andersson', 'Steven Klass', 'sgillis',
+               'techdragon']
 __license__ = 'See the file LICENSE.txt for licensing information.'
