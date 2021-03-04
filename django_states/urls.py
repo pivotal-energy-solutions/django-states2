108e519ff6cc57e219b625e1839a841921de0642
Steven K <steven@pointcircle.com>
2021-03-04 15:12:26 -0700

    re_path to path

--- django_states/urls.py
+++ django_states/urls.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """Urls"""
 
-from django.urls import re_path
+from django.urls import path
 from django_states.views import make_state_transition
 
 urlpatterns = [
-    re_path(r'^make-state-transition/$', make_state_transition,
-            name='django_states_make_transition'),
+    path('make-state-transition', make_state_transition, name='django_states_make_transition'),
 ]
